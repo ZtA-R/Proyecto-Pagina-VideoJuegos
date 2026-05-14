14cc850180a379626b88841792322563a09401f4
Pere Miquel <peremiquel777@gmail.com>
2026-05-14 10:02:53 +0200

    Select a producto + productes.js ordenar

--- projecteVideojocs_front/js/productes.js
+++ projecteVideojocs_front/js/productes.js
@@ -1,10 +1,68 @@
 import {consultar} from "./api.js";
+let prova = await obtenirJocsDB("AVG(val.puntuacio) DESC");
+console.log(prova);
 
-let res = await obtenirTotsJocs();
-console.log(res)
+const select = document.querySelector("#ordenacio");
+select.addEventListener("change", async function(event){
+    const tipusOrdenacio = event.target.value;
+    let res;
 
-function obtenirTotsJocs () {
-    const query = "SELECT v.titol, v.preu, AVG(val.puntuacio) AS puntuacio_mitjana, GROUP_CONCAT(DISTINCT c.nom, ' ', c.cognom SEPARATOR ', ') AS creadors FROM videojoc v LEFT JOIN valoracio val ON val.id_videojoc = v.id LEFT JOIN videojoc_creador vc ON vc.id_videojoc = v.id LEFT JOIN creador c ON c.id = vc.id_creador GROUP BY v.id, v.titol, v.preu;"
+    switch(tipusOrdenacio) {
+        case "puntuacio_desc":
+                res = await obtenirJocsDB("AVG(val.puntuacio) DESC");
+                break;
+        case "puntuacio_asc":
+                res = await obtenirJocsDB("AVG(val.puntuacio) ASC");
+                break;
+        case "alfabetic_asc":
+                res = await obtenirJocsDB("v.titol ASC");
+                break;
+        case "alfabetic_desc":
+                res = await obtenirJocsDB("v.titol DESC");
+                break;
+        case "preu_asc":
+                res = await obtenirJocsDB("v.preu ASC");
+                break;
+        case "preu_desc":
+                res = await obtenirJocsDB("v.preu DESC");
+                break;
+        default:
+            res = await obtenirJocsDB("AVG(val.puntuacio) DESC");    
+        }
+
+        const divPrincipal = document.querySelector("#llista_videojocs");
+        divPrincipal.innerHTML = "";
+
+        res.forEach(function(r){
+            crearContenidor(r.titol, r.preu, r.puntuacio_mitjana, r.creadors);
+        });
+});
+
+function obtenirJocsDB (ordenacio) {
+    const query = `SELECT v.titol, v.preu, AVG(val.puntuacio) AS puntuacio_mitjana, GROUP_CONCAT(DISTINCT c.nom, ' ', c.cognom SEPARATOR ', ') AS creadors FROM videojoc v LEFT JOIN valoracio val ON val.id_videojoc = v.id LEFT JOIN videojoc_creador vc ON vc.id_videojoc = v.id LEFT JOIN creador c ON c.id = vc.id_creador GROUP BY v.id, v.titol, v.preu ORDER BY ${ordenacio};`
 
     return consultar(query);
+}
+
+
+function crearContenidor (titol, preu, puntuacio_mitjana, creador) {
+    const v_div = document.createElement("div");
+    
+    const v_titol = document.createElement("p");
+    v_titol.textContent = titol;
+    
+    const v_preu = document.createElement("p");
+    v_preu.textContent = preu;
+    
+    const v_puntuacio = document.createElement("p");
+    v_puntuacio.textContent = puntuacio_mitjana;
+    
+    const v_creador = document.createElement("p");
+    v_creador.textContent = creador;
+
+    document.querySelector("#llista_videojocs").appendChild(v_div);
+    v_div.appendChild(v_titol);
+    v_div.appendChild(v_preu);
+    v_div.appendChild(v_puntuacio);
+    v_div.appendChild(v_creador);
 }
